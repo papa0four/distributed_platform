af9d899fa08fa192edea9eb5f9051c6f03693a19
defender <defender@pop-os.localdomain>
2021-07-12 20:40:27 -0400

    added lots of functionality to include proper cmdline parsing and formulation of job packet

--- src/submitter/submitter.py
+++ src/submitter/submitter.py
@@ -2,7 +2,7 @@
 
 import argparse
 import re
-from struct import pack
+from typing import Tuple
 
 if __name__ == "__main__" and __package__ is None:
     from sys import path
@@ -15,6 +15,8 @@ import modules.connect_to_scheduler as cts
 import modules.help_msg as hm
 import modules.job as job
 import modules.pack_protocols as p_protocols
+
+serv_info: Tuple = ()
 
 def test_num_sz(num: int) -> bool:
     """
@@ -102,37 +104,83 @@ def valid_opchain_check(op_chain: str) -> list:
         chain_of_ops.append(op_group)
     return chain_of_ops
 
-
-
-def handle_cmdline_arguments() -> list:
+def create_protocol_header(version: int, operation: int) -> Tuple:
     """
     docstring goes here
     """
-    cts.connect_to_scheduler()
-    parser = argparse.ArgumentParser(description='enter operation chain to be processed.')
-    parser.add_argument('-n', dest="options", nargs='+', help='comma delimited'\
-                        'positive integers\n\tcan be singular or a valid range separated by'\
-                        ' a hyphen')
-    parser.add_argument('-s', dest="shutdown", type=None, nargs='?', help='meant to shutdown the '\
-                        'scheduler\n\t does not take any arguments')
-    args = parser.parse_args()
-    return args
+    header: Tuple = ()
+
+    protocol_header = p_protocols.Packet_Protocol(version, operation)
+    job_packet = p_protocols.Submit_Job()
+
+    header = job_packet.convert_to_bytes(protocol_header.version)
+    header += job_packet.convert_to_bytes(protocol_header.operation)
+    return header
+
+def create_payload(num_ops: int, op_chain: list, iter: int, 
+                    num_items: int, items: list) -> bytes:
+    """
+    docstring goes here
+    """
+    job_packet = p_protocols.Submit_Job()
+    payload = job_packet.convert_to_bytes(int(num_ops))
+
+    for _, group in enumerate(op_chain):
+        operation = group[0]
+        operand = group[1]
+        payload += job_packet.convert_to_bytes(operation)
+        payload += job_packet.convert_to_bytes(operand)
+
+    payload += job_packet.convert_to_bytes(int(iter))
+    payload += job_packet.convert_to_bytes(int(num_items))
+
+    for operand in items:
+        payload += job_packet.convert_to_bytes(int(operand))
+
+    return payload
 
 def handle_submitter() -> None:
     """
     docstring goes here
     """
-    args = handle_cmdline_arguments()
+    protocol_version = 1
+    submit_job = 0
+    iterations = 1
+    conn_fd = cts.connect_to_scheduler()
+    parser = argparse.ArgumentParser(description='enter operation chain to be processed.')
+    parser.add_argument('-n', dest="options", nargs='+', help='comma delimited'\
+                        'positive integers\n\tcan be singular or a valid range separated by'\
+                        ' a hyphen')
+    parser.add_argument('-s', dest="shutdown", action='store_true', help='meant to shutdown the '\
+                        'scheduler\n\t does not take any arguments')
+    args = parser.parse_args()
+    
+    if args.shutdown == True:
+        shutdown = 5
+        header = create_protocol_header(protocol_version, shutdown)
+        print(f"sending shutdown flag...")
+        exit()
+
     operation_list = args.options[0].split(' ')
     operation_chain = args.options[1].split(' ')
     if args.options == None or len(args.options) != 2:
         hm.usage_msg()
         exit()
+
     op_list = valid_operand_check(operation_list[0])
-    print("operands:")
-    print("\t", op_list)
     op_chain = valid_opchain_check(operation_chain[0])
-    print("op_chain:")
-    print("\t", op_chain)
+    header = create_protocol_header(protocol_version, submit_job)
+    payload = create_payload(len(op_chain), op_chain, iterations, 
+                len(op_list), op_list)
+    packet = header + payload
+    try:
+        bytes_sent = conn_fd.send(packet)
+        if bytes_sent <= 0:
+            print("no bytes sent to scheduler")
+        print(f"job submitted {len(packet)} bytes sent ...")
+    except IOError as send_err:
+        print("send error", send_err)
+        conn_fd.close()
+        exit()
 
 handle_submitter()
